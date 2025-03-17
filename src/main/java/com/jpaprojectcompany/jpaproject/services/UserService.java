fcf089f202b28ea40f569174c4a3e1fc95f1c99b
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2025-03-17 16:41:48 -0300

    Service layer, component registration

---
+++ src/main/java/com/jpaprojectcompany/jpaproject/services/UserService.java
@@ -0,0 +1,30 @@
+package com.jpaprojectcompany.jpaproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jpaprojectcompany.jpaproject.entities.User;
+import com.jpaprojectcompany.jpaproject.repositories.UserRepository;
+
+// COMPONENT REGISTRATION
+// @Service, @Repository
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository ur;
+	
+	public List<User>findAll(){
+		return ur.findAll();
+	}
+	
+	public User findById(Long id) {
+		// Optional = PODE RETORNAR ALGO OU NÃO
+		Optional<User> object = ur.findById(id);
+		return object.get();
+	}
+}
