879a7fc741bddbc4f17edcb066c254da13f9c063
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2025-03-17 16:57:00 -0300

    Order mapping part 1

--- src/main/java/com/jpaprojectcompany/jpaproject/entities/User.java
+++ src/main/java/com/jpaprojectcompany/jpaproject/entities/User.java
@@ -1,12 +1,15 @@
 package com.jpaprojectcompany.jpaproject.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +27,13 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	public ArrayList<Order> getOrders(){
+		return (ArrayList<Order>) orders;
+	}
 	
 	public User() {}
 
